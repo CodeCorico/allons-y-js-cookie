afbb3988c8ac24d7823d4691fb3ced980c50849e
Xavier Boubert <xavier@boubert.fr>
2016-10-06 11:11:29 +0200

    feat(js-cookie): create the feature

---
+++ features/js-cookie/js-cookie-gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = function($allonsy, $gulp) {
+  var sourcemaps = require('gulp-sourcemaps'),
+      uglify = require('gulp-uglify'),
+      rename = require('gulp-rename');
+
+  $gulp.task('js-cookie', function(done) {
+
+    $gulp
+      .src('node_modules/js-cookie/src/js.cookie.js')
+      .pipe($gulp.dist('vendor'))
+      .pipe(sourcemaps.init())
+      .pipe(uglify().on('error', function(err) {
+        $allonsy.logWarning('allons-y-js-cookie', 'js-cookie:uglify', {
+          error: err
+        });
+
+        this.emit('end');
+      }))
+      .pipe(rename({
+        extname: '.min.js'
+      }))
+      .pipe(sourcemaps.write('./'))
+      .pipe($gulp.dist('vendor'))
+      .on('end', done);
+  });
+
+  return 'js-cookie';
+};
